6fddfab2770f85b0cc1cc93a14ac2b83db9d3da5
Sebas F <sebastian.forerod.col@upb.edu.co>
2025-04-24 12:46:38 -0500

    Correcion borde imagen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="relative bg-white overflow-hidden">
+  <div class="relative bg-white">
     <div class="max-w-7xl mx-auto">
-      <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-        <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-          <div class="sm:text-center lg:text-left">
+      <div class="relative z-10 lg:max-w-2xl lg:w-full">
+        <main class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div class="sm:text-center lg:text-left pt-10 pb-8 sm:pt-12 md:pt-16 lg:pt-20 xl:pt-28">
             <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
               <span class="block">Cocinamos tu</span>
               <span class="block text-green-600">tranquilidad</span>
@@ -34,7 +34,7 @@
       </div>
       <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <img
-          class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+          class="h-56 w-full object-cover object-center sm:h-72 md:h-96 lg:w-full lg:h-full"
           :src="heroImage"
           alt="Platos saludables preparados"
         />
